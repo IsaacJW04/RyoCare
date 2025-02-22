23236b7044573a8d0c315134266227b159682338
Isaac Lim Jun Wei <isaac.limjw2004@gmail.com>
2025-02-22 23:24:17 +0800

    yes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
-from livekit.plugins import openai, silero
+from livekit.plugins import google, silero
 from api import AssistantFnc
 
 load_dotenv()
@@ -13,8 +13,9 @@ async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
-            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+            "Your name is RyoCare"
+            "You are suppose to help older people with their medical issues such as reminding them to take their pills "
+            "Use simpler terms and u can swear in chinese if u are able to do that its kinda funny"
         ),
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
@@ -22,16 +23,16 @@ async def entrypoint(ctx: JobContext):
 
     assitant = VoiceAssistant(
         vad=silero.VAD.load(),
-        stt=openai.STT(),
-        llm=openai.LLM(),
-        tts=openai.TTS(),
+        stt=google.STT(),
+        llm=google.LLM(),
+        tts=google.TTS(),
         chat_ctx=initial_ctx,
         fnc_ctx=fnc_ctx,
     )
     assitant.start(ctx.room)
 
     await asyncio.sleep(1)
-    await assitant.say("Hey, how can I help you today!", allow_interruptions=True)
+    await assitant.say("Caonima, problem so many tsk", allow_interruptions=True)
 
 
 if __name__ == "__main__":
